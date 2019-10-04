b292c27375efdbce2710f8e5156ea3fcf4cb30ee
ahmadansori510 <ahmadansori510@gmail.com>
2019-10-04 10:29:44 +0700

    first commit

---
+++ resources/views/layout/frontend/master.blade.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>SMK N 1 BANGSRI</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <!-- styles -->
+  <link href="assets/css/bootstrap.css" rel="stylesheet" />
+  <link href="assets/css/bootstrap-responsive.css" rel="stylesheet" />
+  <link href="assets/css/prettyPhoto.css" rel="stylesheet" />
+  <link href="assets/css/animate.css" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css?family=Noto+Serif:400,700,400italic|Roboto+Condensed:400,300,700" rel="stylesheet" />
+
+  <link href="assets/css/style.css" rel="stylesheet" />
+  <link href="assets/color/default.css" rel="stylesheet" />
+
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png" />
+  <link rel="shortcut icon" href="ico/favicon.ico" />
+
+  <!-- =======================================================
+    Theme Name: Vesperr
+    Theme URL: https://bootstrapmade.com/vesperr-free-bootstrap-template/
+    Author: BootstrapMade.com
+    Author URL: https://bootstrapmade.com
+  ======================================================= -->
+</head>
+
+<body>
+  <header>
+    <!-- start top -->
+    <div id="topnav" class="navbar navbar-fixed-top default">
+      <div class="navbar-inner">
+        <div class="container">
+          <div class="logo">
+            <img src="assets/img/4.jpg"  style="font-family:ALGERIAN;"/>     SMK BISA      </a>
+             
+          </div>
+          <div class="navigation">
+            <nav>
+              <ul class="nav pull-right">
+                <li class="current"><a href="#intro">HOME</a></li>
+                <li><a href="#about">PROFIL SEKOLAH</a></li>
+                <li><a href="#services">JURUSAN</a></li>
+                <li><a href="#works">GALERY</a></li>
+                <li><a href="#contact">CONTACT</a></li>
+                
+              </ul>
+            </nav>
+          </div>
+          <!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+    <!-- end top -->
+  </header>
+  <!-- section intro -->
+  <section id="intro">
+    <div class="slogan">
+      
+      <h1>SMK NEGERI 1 BANGSRI</h1>
+      <h2 style="font-family:Goudy Old Style;">Welcome to the website SMK N 1 BANGSRI</h2>
+    </div>
+  </section>
+  <!-- end intro -->
+
+@yield('content')
+
+  <footer>
+    <div class="verybottom">
+      <div class="container">
+        <div class="row">
+          <div class="span12">
+            <div class="aligncenter">
+              <div class="logo">
+                <a class="brand" href="index.html"><img src="assets/img/4.jpg" alt="" /></a>
+              </div>
+              <p>
+                <h2 style="font-family:ALGERIAN;background-color: black;">SMK N 1 BANGSRI</h2></a>
+              </p>
+              <div class="credits">
+                <!--
+                  All the links in the footer should remain intact.
+                  You can delete the links only if you purchased the pro version.
+                  Licensing information: https://bootstrapmade.com/license/
+                  Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Vesperr
+                -->
+               
+
+</body>
+
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,377 @@
+@extends('layout.frontend.master')
+@section('content')
+
+  <!-- Section about -->
+  <section id="about" class="section">
+    <div class="container">
+      <div class="row">
+        <div class="span12">
+          <div class="heading">
+            <a href="index.html"><img src="assets/img/1.jpg" alt="" /></a>
+             <a class="navbar-brand" href="#">
+                            <h2 style="font-family:ALGERIAN;background-color: black;">SMK N 1 BANGSRI</h2></a>
+          </div>
+
+ <div class="heading">
+             <a class="navbar-brand" href="#">
+                            <h2 style="font-family:ALGERIAN;background-color: red;">IDENTITAS SEKOLAH</h2></a>
+                             <p>NPSN : 20360586<br>
+Status : Negeri<br>
+Bentuk Pendidikan : SMK<br>
+Status Kepemilikan : Pemerintah Daerah<br>
+SK Pendirian Sekolah : 29 TAHUN 2008<br>
+Tanggal SK Pendirian : 2008-11-20<br>
+SK Izin Operasional : 29 TAHUN 2008<br>
+Tanggal SK Izin Operasional : 2008-11-20<br></p>
+
+
+<div class="heading">
+             <a class="navbar-brand" href="#">
+                            <h2 style="font-family:ALGERIAN;background-color: red;">DATA PELENGKAP</h2></a>
+                            <p>Kebutuhan Khusus Dilayani : Tidak ada<br>
+
+Nama Bank : SIMPEDA HIPPRADA<br>
+
+Cabang KCP/Unit : CAPEM PASAR BANGSRI<br>
+
+Rekening Atas Nama : SMKN 1 BANGSRI JEPARA<br>
+
+Luas Tanah Milik : 10913<br>
+
+Luas Tanah Bukan Milik : 0<br></p>
+
+ <div class="heading">
+             <a class="navbar-brand" href="#">
+                            <h2 style="font-family:ALGERIAN;background-color: red;">DATA RINCI</h2></a>
+
+                                                        <p>Status BOS : Bersedia Menerima<br>
+
+Waku Penyelenggaraan : Sehari penuh (5 h/m)<br>
+
+Sertifikasi ISO : 9001:2008<br>
+
+Sumber Listrik : PLN<br>
+
+Daya Listrik : 54000<br>
+
+Akses Internet : Telkomsel Flash<br></p>
+
+
+<div class="heading">
+            
+             <a class="navbar-brand" href="#">
+                            <h2 style="font-family:ALGERIAN;background-color: black;"></h2></a>
+          </div>
+
+<a class="navbar-center=0 auto" href="#">
+                                 <h2 style="font-family:ALGERIAN;background-color: black;">SMK BISA</h2></a>                               </div>
+
+
+<div class="heading">
+            <a href="index.html"><img src="assets/img/1.jpg" alt="" /></a>
+             <a class="navbar-brand" href="#">
+                            <h2 style="font-family:ALGERIAN;background-color: black;">SMK N 1 BANGSRI</h2></a>
+          </div>
+
+
+
+<div class="section-heading text-center">
+                                
+                                <a class="navbar-center=0 auto" href="#">
+                                <h2 style="font-family:ALGERIAN;background-color: red;">VISI</h2>
+                                <p style="font-family:ALGERIAN;">TERWUJUDNYA SMK YANG MAMPU MENCIPTAKAN TAMATAN YANG CERDAS,PRODUKTIF,KOMPETITIF, BERTAQWA, SERTA BERAKHLAK MULIA.<br></p>
+
+                                
+                                <a class="navbar-center=0 auto" href="#">
+                                 <h2 style="font-family:ALGERIAN;background-color: red;">MISI</h2>
+                                <p style="font-family:ALGERIAN;">1.MELAKSANAKAN PEMBELAJARAN YANG BERTARAF NASIONAL DAN INTARNASIONAL BERBSIAS KEUNGGULAN SENI DAN BUDAYA.<br>
+                                2.MENJALIN KERJA SAMA DENGAN DUNIA USAHA / DUNIA INDUSTRI DAN STAKOHOLDER.<br>
+                                3.MEMUWUJUDKAN WAWASAN WIYATA MANDALA DAN MENINGKATKAN CITRA SEKOLAH MENENGAH KEJURUAN.<br>
+                                4.MENGHASILKAN TAMATAN YANG CERDAS, PRODUKTIF, BETAQWA, DAN BERAKHLAK MULIA.<br>
+
+                                <a class="navbar-center=0 auto" href="#">
+                                 <h2 style="font-family:ALGERIAN;background-color: black;">SMK BISA</h2></a>                               </div> 
+
+
+<div class="heading">
+            <a href="index.html"><img src="assets/img/1.jpg" alt="" /></a>
+             <a class="navbar-brand" href="#">
+                            <h2 style="font-family:ALGERIAN;background-color: black;">SMK N 1 BANGSRI</h2></a>
+          </div>
+
+
+<a class="navbar-center=0 auto" href="#">
+                                <h2 style="font-family:ALGERIAN;background-color: red;"></h2></a>
+                               <h2 style="font-family:ALGERIAN;"><span>REKAPITULASI SEKOLAH</span></h2>
+
+
+
+ <div class="alert alert-info" role="alert">
+                                          Data PTK dan PD
+                                    </div>
+
+ <p>Uraian   Guru    Tendik  PTK PD<br>
+                                       Laki-laki   16  11  27  536<br>
+                                       Perempuan   18  5   23  611<br>
+                                       Total       34  16  50  1147<br>Keterangan :<br></p>
+                                       
+ - Data Rekap Per Tanggal 1 Oktober 2019<br>
+ - Penghitungan PTK adalah yang sudah mendapat penugasan, berstatus aktif dan terdaftar di sekolah induk.<br>
+ - Singkatan :<br>
+1.PTK = Guru ditambah Tendik<br>
+2.PD = Peserta Didik<br></p>
+
+
+<div class="alert alert-info" role="alert">
+                                          Data Sarpras
+                                     </div>
+
+ <p>No   Jenis Sarpras   Jumlah<br>
+1   Ruang Kelas 26<br>
+2   Ruang Laboratorium  5<br>
+3   Ruang Perpustakaan  1<br>
+Total   32<br></p>
+
+
+ <div class="alert alert-info" role="alert">
+                                          Data Sanitasi
+                                     </div>
+                                    <p>No   Nama Variabel   Uraian<br>
+1   Kecukupan air   Cukup<br>
+2   Sekolah memproses air sendiri   Ya<br>
+3   Air minum untuk siswa   Tidak disediakan<br>
+4   Mayoritas membawa air minum Tidak<br>
+5   Jumlah toilet berkebutuhan khusus   0<br>
+6   Sumber air sanitasi Ledeng/PAM<br>
+7   Ketersediaan air di lingkungan sekolah  Ya<br>
+8   Tipe jamban Leher angsa (toilet duduk/jongkok)<br>
+9   Apakah sabun dan air mengalir pada tempat cuci tangan   Ya<br>
+10  Jamban dapat digunakan  10<br>
+11  Jamban tidak dapat digunakan    0<br></p>
+
+
+
+<div class="alert alert-info" role="alert">
+                                         Data Rombongan Belajar
+                                     </div>
+                                    <p>SMK NEGERI 1 BANGSRI memiliki jumlah rombel sebanyak 30, dengan uraian sebagai berikut:<br>
+
+Uraian  Rombel 10   Rombel 11   Rombel 12<br>
+L   P   Tot L   P   Tot L   P   Tot<br>
+Jumlah  195 197 392 156 211 367 127 161 288<br>
+</p>
+
+
+                      
+          </div>
+
+          
+          
+  <!-- end section about -->
+  <!-- section services -->
+  <section id="services" class="section parallax dark">
+    <div class="container">
+      <div class="row">
+        <div class="span12">
+          <div class="heading">
+            <h3><span><h2 style="font-family:ALGERIAN;">JURUSAN</h2></span></h3>
+          </div>
+         
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+      <div class="row">
+        <div class="span3">
+          <div class="box aligncenter">
+            <div class="halftop">
+              <h4><strong>REKAYASA PERANGKAT LUNAK</strong></h4>
+              <i class="icon-desktop icon-4x"></i>
+            </div>
+            <p>
+              SMK BISA<br>
+            </p>
+            <a href="#" class="btn btn-theme">Learn more</a>
+          </div>
+        </div>
+        <div class="span3">
+          <div class="box aligncenter">
+            <div class="halftop">
+              <h4><strong>ADMINISTRAS PERKANTORAN</strong></h4>
+              <i class="icon-briefcase icon-4x"></i>
+            </div>
+            <p>
+              SMK BISA<br>
+            </p>
+            <a href="#" class="btn btn-theme">Learn more</a>
+          </div>
+        </div>
+        <div class="span3">
+          <div class="box aligncenter">
+            <div class="halftop">
+              <h4><strong>PEMASARAN</strong></h4>
+              <i class="icon-mobile-phone icon-4x"></i>
+            </div>
+            <p>
+              SMK BISA<br>
+            </p>
+            <a href="#" class="btn btn-theme">Learn more</a>
+          </div>
+        </div>
+        <div class="span3">
+          <div class="box aligncenter">
+            <div class="halftop">
+              <h4><strong>TEKNIK SEPEDA MONTOR</strong></h4>
+              <i class="icon-beaker icon-4x"></i>
+            </div>
+            <p>
+              SMK BISA<br>
+            </p>
+            <a href="#" class="btn btn-theme">Learn more</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+   
+
+
+  </section>
+  <!-- end section services -->
+  <!-- section works -->
+  <section id="works" class="section gray-bg">
+    <div class="container">
+      <div class="row">
+        <div class="span12">
+          <div class="heading">
+            <h3><span>GALERY</span></h3>
+          </div>
+         
+        </div>
+      </div>
+
+<img src="assets/img/portfolio/foto1.jpg">
+<img src="assets/img/portfolio/foto12.jpg">
+<img src="assets/img/portfolio/foto3.jpg">
+<img src="assets/img/portfolio/foto4.jpg">
+<img src="assets/img/portfolio/foto5.jpg">
+<img src="assets/img/portfolio/foto6.jpg">
+<img src="assets/img/portfolio/foto7.jpg">
+<img src="assets/img/portfolio/foto8.jpg">
+<img src="assets/img/portfolio/foto9.jpg">
+<img src="assets/img/portfolio/foto10.jpg">
+<img src="assets/img/portfolio/foto11.jpg">
+<img src="assets/img/portfolio/foto12.jpg">
+<img src="assets/img/portfolio/foto13.jpg">
+
+
+  <!-- section works -->
+
+
+
+
+  <!-- section contact -->
+  <section id="contact" class="section">
+    <div class="container">
+      <div class="row">
+        <div class="span12">
+          <div class="heading">
+            <h3><span>CONTACT</span></h3>
+          </div>
+          
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div class="row">
+        <div class="span6">
+          <h4><i class="icon-envelope"></i><strong>Contact form</strong></h4>
+         
+          <!-- start contact form -->
+          <form action="" method="post" role="form" class="contactForm">
+            <div id="sendmessage">Your message has been sent. Thank you!</div>
+            <div id="errormessage"></div>
+
+            <ul class="contact-list">
+              <li class="form-group">
+                <label>Name <span>*</span></label>
+                <input type="text" name="name" class="form_input" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                <div class="validation"></div>
+                <li class="form-group">
+                  <label>Email <span>*</span></label>
+                  <input type="email" class="form_input" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                  <div class="validation"></div>
+                </li>
+                <li class="form-group">
+                  <label>Subject <span>*</span></label>
+                  <input type="text" class="form_input" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                  <div class="validation"></div>
+                </li>
+
+                <li class="form-group">
+                  <label>Your message <span>*</span></label>
+                  <textarea class="form_textarea" name="message" rows="12" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                  <div class="validation"></div>
+                </li>
+                <li class="last">
+                  <button class="btn btn-large btn-theme" type="submit" id="send">Send a message</button>
+                </li>
+            </ul>
+          </form>
+
+          <!-- end contact form -->
+        </div>
+        
+          <h4><i class="icon-group"></i><strong>Connect to our social profiles</strong></h4>
+          <div class="social-links">
+            <ul class="social-links">
+              <li><a href="#" title="Twitter"><i class="icon-square icon-32 icon-twitter"></i></a></li>
+              <li><a href="#" title="Facebook"><i class="icon-square icon-32 icon-facebook"></i></a></li>
+              <li><a href="#" title="Google plus"><i class="icon-square icon-32 icon-google-plus"></i></a></li>
+              <li><a href="#" title="Linkedin"><i class="icon-square icon-32 icon-linkedin"></i></a></li>
+              <li><a href="#" title="Pinterest"><i class="icon-square icon-32 icon-pinterest"></i></a></li>
+            </ul>
+          </div>
+
+ <div class="subfeature">
+                            <div class="col-md-12 col-sm-12 col-xs-12">
+                                <h1 style="font-family:ALGERIAN;"><span>CONTACT</span></h1>
+                            </div>
+                        </div>
+                        <p>Alamat : JL. KH. ACHMAD FAUZAN NO. 17 KRASAK<br>
+
+RT / RW : 5 / 16<br>
+
+Dusun : Kedung Lesung<br>
+
+Desa / Kelurahan : BANGSRI<br>
+
+Kecamatan : Kec. Bangsri<br>
+
+Kabupaten : Kab. Jepara<br>
+
+Provinsi : Prov. Jawa Tengah<br>
+
+Kode Pos : 59453<br>
+
+Lintang : -6.5272000<br>
+
+Bujur : 110.7509000<br></p>
+
+         
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end section contact -->
+
+@stop
